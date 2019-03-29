8e0b593e14cc43740c4ac1272effb851eb1aa9ca
Gabriel Jealmo <gabrieljealmo@gmail.com>
2019-03-29 12:59:30 +0100

    Startpage fix

--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -11,15 +11,17 @@ export default class StartPage extends React.Component{
         console.log(data);
 
         let auctions = data.map(auction => 
-                <div class="auction-item" key={auction.AuktionID}><p>{auction.Titel}</p></div>
+                <div className="auction-item" key={auction.AuktionID}><p>{auction.Titel}</p></div>
             );
         return(
-            <div>
-                <h1 id="StartPage-h1">Välkommen till Nackowskis – en av Världens största marknadsplatser</h1>
-                <p id="p-tag-under-h1">Här shoppar du hållbart bland miljontals unika saker och fynd</p>
-                <h2>Trendar på Nackowskis</h2>
-                <div class="auction-items-container">
-                    {auctions}
+            <div className="startPage-Items">
+                <div className="items">
+                    <h1 id="StartPage-h1">Välkommen till Nackowskis – en av Världens största marknadsplatser</h1>
+                    <p id="p-tag-under-h1">Här shoppar du hållbart bland miljontals unika saker och fynd</p>
+                    <h2>Trendar på Nackowskis</h2>
+                    <div className="auction-items-container">
+                        {auctions}
+                    </div>
                 </div>
             </div>
         );
