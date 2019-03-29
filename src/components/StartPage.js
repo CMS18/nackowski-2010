d537a96205505bcd24784439673f26e24cfc34e1
Michael Fors <erik.michael.fors@gmail.com>
2019-03-29 15:07:15 +0100

    Added design for radio buttons ala gabbe

--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -4,6 +4,30 @@ export default class StartPage extends React.Component{
     constructor(props){
         super(props);
         this.state = {  };
+        this.handleDateChoice = this.handleDateChoice.bind(this);
+    }
+
+    handleDateChoice(e){
+        let radioOne = document.getElementById('ValidDates');
+        let radioTwo = document.getElementById('InvalidDates');
+
+        if(radioOne.style.backgroundColor !== "black"){
+            radioOne.style.backgroundColor = "black";
+            radioOne.style.color = "white";
+            radioTwo.style.backgroundColor = "white";
+            radioTwo.style.color = "black";
+
+            //DateSort Current
+        }
+        else{
+            radioOne.style.backgroundColor = "white";
+            radioOne.style.color = "black";
+            radioTwo.style.backgroundColor = "black";
+            radioOne.style.color = "white";
+
+            //DateSort Old
+        }
+        
     }
 
     render(){
@@ -19,6 +43,18 @@ export default class StartPage extends React.Component{
                     <h1 id="StartPage-h1">Välkommen till Nackowskis – en av Världens största marknadsplatser</h1>
                     <p id="p-tag-under-h1">Här shoppar du hållbart bland miljontals unika saker och fynd</p>
                     <h2>Trendar på Nackowskis</h2>
+                    <div className="dateChoices">
+                        <p className="currentText">Current Auctions</p>
+                        <p className="oldText">Old Auctions</p>
+                    </div>
+                    <div className="dateCheck">
+                        <div id="ValidDates" className="validDates">
+                            <input id="Valid" onClick={this.handleDateChoice} className="radioButtons" type="radio" name="date" defaultChecked />
+                        </div>
+                        <div id="InvalidDates" className="invalidDates">
+                            <input id="Invalid" onClick={this.handleDateChoice} className="radioButtons" type="radio" name="date" />
+                        </div>
+                    </div>
                     <div className="auction-items-container">
                         {auctions}
                     </div>
