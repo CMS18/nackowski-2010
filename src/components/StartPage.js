0631c7d3a9e31e309e2b2fa195163c467d3d26f3
Michael Fors <erik.michael.fors@gmail.com>
2019-03-29 10:50:49 +0100

    StartPage Done-ish

--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -1,29 +1,75 @@
 import React from 'react';
 
 async function GetAuctions(){
-    let url = "http://nackowskis.azurewebsites.net/api/Auktion/7";
+    let url = "http://nackowskis.azurewebsites.net/api/Auktion/1";
     return await fetch(url).then(function(response){ return response.json(); });
+}
+
+async function GetBids(auktionsId){
+    let url = "http://nackowskis.azurewebsites.net/api/Bud/1/"+auktionsId+"";
+    return await fetch(url).then(function(response){ return response.json(); });
+}
+
+function getTrendingAuctions(state){
+        
+    GetAuctions().then(function(promise){ return promise; }).then(data => state.bids = data );
+
+    let data = state.bids;
+
+    let auctionIds = [];
+    for (let i=0; i<data.length; i++){
+        auctionIds.push(data[i].AuktionID);
+    }
+
+    let newData = [];
+    for (let i=0; i<auctionIds.length; i++){
+        newData.push(GetBids(auctionIds[i]));
+    }
+    return newData;
 }
 
 export default class StartPage extends React.Component{
     constructor(props){
         super(props);
-        this.state = { auctions: [] };
+        this.state = {
+            auctions: [],
+            bids: []
+        };
+    }
+
+    getMostUrgentAuctions(){
+
+    }
+
+    getOldestAuctions(){
+        return null;
     }
 
     render(){
+        /**let data;
+        if(this.state.auctions.length === 0){
+            data = getTrendingAuctions(this.state);
+        } */
         if(this.state.auctions.length === 0){
             GetAuctions().then(function(promise){ return promise; }).then(data => this.setState({ auctions: data }));
         }
         let data = this.state.auctions;
+        
+        
         console.log(data);
+
         let auctions = data.map(auction => 
-                <li key={auction.AuktionID}>{auction.Titel}</li>
+                <div class="auction-item" key={auction.AuktionID}><p>{auction.Titel}</p></div>
             );
         return(
-            <ul>
-                {auctions}
-            </ul>
+            <div>
+                <h1 id="StartPage-h1">Välkommen till Nackowskis – en av Världens största marknadsplatser</h1>
+                <p id="p-tag-under-h1">Här shoppar du hållbart bland miljontals unika saker och fynd</p>
+                <h2>Trendar på Nackowskis</h2>
+                <div class="auction-items-container">
+                    {auctions}
+                </div>
+            </div>
         );
     }
 }
