9d5c43bfaae10cb8178f83761eb60f372acd12e6
Linnea <linnea.eriksson91@hotmail.com>
2019-04-02 11:16:39 +0200

    Started on DetailView

--- src/AuctionApp.js
+++ src/AuctionApp.js
@@ -7,30 +7,51 @@ import Navbar from './components/Navbar';
 import { DetailView } from './components/DetailView';
 import { APIModule } from './modules';
 
-export default class AuctionApp extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = { auctions: [], searchValue: "", detailViewRoutes: [] };
-        this.onChange = this.onChange.bind(this);
-    }
+export default class AuctionApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { auctions: [], searchValue: '', detailViewRoutes: [] };
+    this.onChange = this.onChange.bind(this);
+  }
 
-    componentDidMount(){
-        APIModule.GetAuctions().then(function(promise){ return promise; }).then(data => this.setState({ auctions: data }));
-    }
+  componentDidMount() {
+    APIModule.GetAuctions()
+      .then(function(promise) {
+        return promise;
+      })
+      .then(data => this.setState({ auctions: data }));
+  }
 
-    onChange(value){
-        this.setState({ searchValue: value });
-    }
+  onChange(value) {
+    this.setState({ searchValue: value });
+  }
 
-    render(){
-        return (
-            <BrowserRouter>
-                <Navbar searchValue={this.state.searchValue} onChange={this.onChange} />
-                <Route exact path="/" component={() => <StartPage auctions={this.state.auctions} />} />
-                {this.state.auctions.map((auction) => ( <Route path={"/DetailView/" + auction.Titel} component={() => <DetailView auctionID={auction.AuktionID} />} /> ))}
-                <Route path="/CreateAuction" component={() => <CreateAuction />} />
-                <Route path="/Search" component={() => <Search auctions={this.state.auctions} searchValue={this.state.searchValue} />} />
-            </BrowserRouter>
-        );
-    }
+  render() {
+    return (
+      <BrowserRouter>
+        <Navbar searchValue={this.state.searchValue} onChange={this.onChange} />
+        <Route
+          exact
+          path="/"
+          component={() => <StartPage auctions={this.state.auctions} />}
+        />
+        {this.state.auctions.map(auction => (
+          <Route
+            path={'/DetailView/' + auction.Titel}
+            component={() => <DetailView auction={auction} />}
+          />
+        ))}
+        <Route path="/CreateAuction" component={() => <CreateAuction />} />
+        <Route
+          path="/Search"
+          component={() => (
+            <Search
+              auctions={this.state.auctions}
+              searchValue={this.state.searchValue}
+            />
+          )}
+        />
+      </BrowserRouter>
+    );
+  }
 }
